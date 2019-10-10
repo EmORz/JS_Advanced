966235e93ebdea8aad705be9c914239777511322
Emo <eiv.razgrad@gmail.com>
2019-10-10 11:51:26 +0300

    add

--- DOM/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/07. Locked-Profile/app.js
+++ DOM/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/07. Locked-Profile/app.js
@@ -1,3 +1,32 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    let btn = [...document.getElementsByTagName('button')];
+   // debugger
+
+    btn.forEach(b => b.addEventListener('click', action));
+
+    function action(event) {
+        let button = event.target;
+        let profile = button.parentNode;
+        
+        let moreInf = profile.getElementsByTagName('div')[0];
+        //debugger
+        let infFromRadioBtn = profile.querySelector('input[type="radio"]:checked').value;
+        console.log(infFromRadioBtn)
+
+        if (infFromRadioBtn === 'unlock') {
+            if (button.textContent === 'Show more') {
+                
+            moreInf.style.display = 'inline-block';
+            button.textContent = 'Hide it';
+            }else if (button.textContent === 'Hide it') {
+                moreInf.style.display = 'none';
+                button.textContent = 'Show more';
+            }
+        }
+
+        
+
+    }
+    //console.log('TODO...')
 }
